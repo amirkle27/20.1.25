1b667f8b96719a65fdef9f36cf9f39c394480c4a
amirkle27 <amirkle@gmail.com>
2025-01-23 14:39:08 +0200

    Update Homework 20.1.25.py

--- Homework 20.1.25.py
+++ Homework 20.1.25.py
@@ -1,6 +1,6 @@
 
 from datetime import datetime,timedelta
-import errors
+from errors import *
 
 class User:
     def __init__(self, name:str, email:str, password:str, birthday:datetime, created_at: datetime = None):
@@ -13,7 +13,7 @@ class User:
 
 
     def __str__(self):
-        return f"name: {self.name}\nemail: {self.email}\npassword: {self.password}\nbirthday: {self.password}\ncreated_at: {self.created_at}"
+        return f"name: {self.name}\nemail: {self.email}\npassword: {self.password}\nbirthday: {self.birthday}\ncreated_at: {self.created_at}"
 
     @property
     def name(self):
@@ -22,9 +22,9 @@ class User:
     @name.setter
     def name(self, name):
         if not len(name) >= 4:
-            raise errors.UserNameTooShortError ("Username too short, must contain at least 4 characters...")
+            raise UserNameTooShortError ("Username too short, must contain at least 4 characters...")
         if not any(char.isalpha() for char in name):
-            raise errors.UserNameNonCharError ("Name must have at least 1 letter")
+            raise UserNameNonCharError ("Name must have at least 1 letter")
         self.__name = name
 
     @property
@@ -34,7 +34,7 @@ class User:
     @email.setter
     def email(self, email):
         if not "@" in email or not "." in email:
-            raise errors.IllegalEmailFormatError("Illegal email address. Email must contain '@' and '.'")
+            raise IllegalEmailFormatError("Illegal email address. Email must contain '@' and '.'")
         self.__email = email
 
     @property
@@ -44,13 +44,13 @@ class User:
     @password.setter
     def password(self,password):
          if not len(password) >= 8:
-             raise errors.IllegalPasswordFormatError("Password must contain at least 8 characters")
+             raise IllegalPasswordFormatError("Password must contain at least 8 characters")
          if not any(char.islower() for char in password):
-             raise errors.IllegalPasswordFormatError("Password must contain at least one lower-case character")
+             raise IllegalPasswordFormatError("Password must contain at least one lower-case character")
          if not any(char.isupper() for char in password):
-             raise errors.IllegalPasswordFormatError("Password must contain at least one higher-case character")
+             raise IllegalPasswordFormatError("Password must contain at least one higher-case character")
          if not any(char in "*&^#$%@!~" for char in password):
-             raise errors.IllegalPasswordFormatError("Password must contain at least one special character (*, &, ^, #, $, %, @, !, ~)")
+             raise IllegalPasswordFormatError("Password must contain at least one special character (*, &, ^, #, $, %, @, !, ~)")
          self.__password = password
 
     @property
@@ -62,9 +62,9 @@ class User:
         if isinstance(birthday, str):
             birthday = datetime.strptime(birthday, '%d/%m/%Y')
         if not birthday <= datetime.now():
-            raise errors.IllegalBirthdayError ("Birthday cannot be in the future!")
+            raise IllegalBirthdayError ("Birthday cannot be in the future!")
         if not (datetime.now()-birthday).days // 365 >= 20:
-            raise errors.UserTooYoungError ("User's age must be above 20!")
+            raise UserTooYoungError ("User's age must be above 20!")
         self.__birthday = birthday
 
     @property
@@ -81,28 +81,62 @@ class User:
     #     created_at = datetime.now()
     #     self.__created_at = created_at
 try:
+
     user = User('Arnon', 'arnon@gmail.com', '1@Asasdsd23', datetime(1990, 1, 1), datetime.now())
-
-    user1 = User('Berta', 'berta12@gmailcom', 'IamBerta!', datetime(1983, 4, 24), datetime.now())
-    user2 = User('Shosh', 'shoshinka@walla.co.il', 'Shosh$$$15', datetime(2022, 14, 8), datetime.now())
-    user3 = User('Gabi', 'ggmail.com', 'IamBerta!', datetime(2083, 10, 6), datetime.now())
-
-    print(user)
-    print(user1)
-    print(user2)
-    print(user3)
-except errors.IllegalPasswordFormatError as e:
+except IllegalPasswordFormatError as e:
     print(f"Password Error: {e}")
-except errors.IllegalEmailFormatError as e:
+except IllegalEmailFormatError as e:
     print(f"Email Error: {e}")
-except errors.UserNameTooShortError as e:
+except UserNameTooShortError as e:
     print(f"Name Error: {e}")
 except Exception as e:
     print(f"An unexpected error occurred: {e}")
-# print(user)
-# # print(user._User__name)
-# # print(user1.email)
-# # print(user1.password)
-# #
-# # print(user1.birthday)
-# # print(user1.created_at)
+
+try:
+    user1 = User('Berta', 'berta12@gmailcom', 'IamBerta!', datetime(1983, 4, 24), datetime.now())
+except IllegalPasswordFormatError as e:
+    print(f"Password Error: {e}")
+except IllegalEmailFormatError as e:
+    print(f"Email Error: {e}")
+except UserNameTooShortError as e:
+    print(f"Name Error: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
+try:
+    user2 = User('Shosh', 'shoshinka@walla.co.il', 'Shosh$$$15', datetime(2022, 14, 8), datetime.now())
+except IllegalPasswordFormatError as e:
+    print(f"Password Error: {e}")
+except IllegalEmailFormatError as e:
+    print(f"Email Error: {e}")
+except UserNameTooShortError as e:
+    print(f"Name Error: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
+try:
+    user3 = User('Gabi', 'ggmail.com', 'IamBerta!', datetime(2083, 10, 6), datetime.now())
+except IllegalPasswordFormatError as e:
+    print(f"Password Error: {e}")
+except IllegalEmailFormatError as e:
+    print(f"Email Error: {e}")
+except UserNameTooShortError as e:
+    print(f"Name Error: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
+
+
+print(user)
+print(user._User__name)
+# print(user1.email)
+# print(user1.password)
+
+# print(user1.birthday)
+# print(user1.created_at)
+
+print(user)
+# print(user1)
+# print(user2)
+# print(user3)
+# print(user.birthday)
